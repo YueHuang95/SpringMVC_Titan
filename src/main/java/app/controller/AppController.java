ba0e1cc69ae831aa5c05eb6863947030b54fee9a
Yue Huang <danhhhuang@163.com>
2019-08-06 16:53:17 +0800

    fixed html page 404 problem

--- src/main/java/app/controller/AppController.java
+++ src/main/java/app/controller/AppController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController {
     @GetMapping("/")
     public String homePage() {
-        return "index";
+        return "/index";
     }
 }
